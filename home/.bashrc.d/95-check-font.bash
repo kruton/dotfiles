a73257c084fee6883569afbbd9f4441c91643f49
Kenny Root <kenny@the-b.org>
2014-01-10 20:07:05 -0800

    Font check works with MacOS / Homebrew

--- home/.bashrc.d/95-check-font.bash
+++ home/.bashrc.d/95-check-font.bash
@@ -1,9 +1,9 @@
 # Checks to see whether PragmataPro font is installed
 
-# This might work on MacOS, but I'd have to test it first.
-if [[ "$OSTYPE" = "linux-gnu" ]]; then
-  if [[ -n "$DISPLAY" ]]; then
-    fc-list -q 'Essential PragmataPro:style=Regular'
+if [[ -n "$DISPLAY" ]]; then
+  FC_LIST=$(which fc-list)
+  if [[ -n ${FC_LIST} ]]; then
+    ${FC_LIST} -q 'Essential PragmataPro:style=Regular'
     if [[ $? -ne 0 ]]; then
       echo ""
       echo "!!! INSTALL Pragmata Pro font to ~/.fonts/"
