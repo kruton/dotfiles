331b0c8e9fb95183876f30412fe41b7102cb292b
Kenny Root <kenny@the-b.org>
2014-01-17 19:03:16 -0800

    rvm: add ruby version manager

---
+++ home/.bashrc.d/60-rvm.bash
@@ -0,0 +1,9 @@
+# Ruby version manager
+
+if [[ -s "$HOME/.rvm/scripts/rvm" ]]; then
+    source "$HOME/.rvm/scripts/rvm"
+fi
+
+if [[ -d $HOME/.rvm/bin ]]; then
+    export PATH=$PATH:$HOME/.rvm/bin
+fi
