0bfb6bd823f8a26c6cf05bbf23107cf4532717f3
Kenny Root <kenny@the-b.org>
2014-02-07 23:32:23 -0800

    Fix Ubuntu bootstrap

--- bootstrap.bash
+++ bootstrap.bash
@@ -16,7 +16,7 @@ case $OSTYPE in
        echo "Unknown Linux distro: lsb_release not found in path!"
        exit 1
     fi
-    distro=${LSBRELEASE}
+    distro=$( ${LSBRELEASE} -si )
     case ${distro} in
        Fedora) platform="fedora" ;;
        Ubuntu) platform="ubuntu" ;;
@@ -33,7 +33,7 @@ case $OSTYPE in
 esac
 
 ### Prepare MacOS X machines ###
-if [[ ${platform} == "darwin" ]]; then
+if [[ ${platform} == darwin ]]; then
   ### Detect XCode tools
   ( gcc -v > /dev/null 2>&1 )
   (( ret = $? ))
@@ -71,8 +71,18 @@ if [[ ${platform} == "darwin" ]]; then
 fi
 
 ### Prepare Fedora machines ###
-if [[ ${platform} == "fedora" ]]; then
+if [[ ${platform} == fedora ]]; then
   sudo yum install python-pip git cmake gcc gcc-c++ python-devel
+fi
+
+### Prepare Ubuntu machines ###
+if [[ ${platform} == ubuntu ]]; then
+  if [[ ! -f /etc/apt-fast.conf ]]; then
+    sudo add-apt-repository ppa:apt-fast/stable
+    sudo apt-get update
+    sudo apt-get install apt-fast
+  fi
+  sudo apt-fast install python-pip git vim cmake python-dev default-jdk
 fi
 
 # if Mac with Homebrew, omit --user
@@ -83,29 +93,35 @@ pip install ${PIPFLAGS} git+https://github.com/kruton/powerline
 
 ### Install homeshick ###
 if [[ ! -e $HOME/.homesick/repos/homeshick ]]; then
-  git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+  git clone https://github.com/andsens/homeshick.git "$HOME/.homesick/repos/homeshick"
 fi
-source $HOME/.homesick/repos/homeshick/homeshick.sh
+source "$HOME/.homesick/repos/homeshick/homeshick.sh"
 
-### Check for private key ###
-if ssh-add -l > /dev/null 2>&1; then
-  repo="git@github.com:kruton/dotfiles"
-else
-  echo ""
-  echo "*** WARNING: Install SSH key later and update dotfiles!"
-  echo ""
-  repo="https://github.com/kruton/dotfiles.git"
+homeshick list | grep -q dotfiles
+if (( $? != 0 )); then
+  ### Check for private key ###
+  if ssh-add -l > /dev/null 2>&1; then
+    repo="git@github.com:kruton/dotfiles"
+  else
+    echo ""
+    echo "*** WARNING: Install SSH key later and update dotfiles!"
+    echo ""
+    repo="https://github.com/kruton/dotfiles.git"
+  fi
+
+  homeshick --batch clone "${repo}"
 fi
-
-homeshick --batch clone "${repo}"
 
 homeshick link --force
 
-mv ~/.bash_history ~/.merged_bash_history
-source ~/.bashrc
+if [[ ! -f $HOME/.merged_bash_history ]]; then
+  mv "$HOME/.bash_history" "$HOME/.merged_bash_history"
+fi
+
+source "$HOME/.bashrc"
 
 vim - +BundleInstall +BundleClean +qall < /dev/null
 
-pushd ~/.vim/bundle/YouCompleteMe
+pushd "$HOME/.vim/bundle/YouCompleteMe"
 [ -x ./install.sh ] && ./install.sh
 popd
