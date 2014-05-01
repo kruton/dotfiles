3d9c497b1e6e5b15b840609ad00d7b63f0ba91c7
Kenny Root <kenny@the-b.org>
2014-05-01 09:50:04 -0700

    bashrc: fix hostname detection

--- home/.bashrc.d/49-set-default-user.bash
+++ home/.bashrc.d/49-set-default-user.bash
@@ -1,5 +1,5 @@
 # This just makes my username disappear with agnoster
-if [[ $HOSTNAME == kroot.* ]]; then
+if [[ $HOSTNAME == kroot* ]]; then
     export DEFAULT_USER="kroot"
 else
     export DEFAULT_USER="kenny"
