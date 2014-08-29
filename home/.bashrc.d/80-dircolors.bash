e30fe9dda6d50abe54817467add009e26f2c94af
Kenny Root <kenny@the-b.org>
2014-08-28 22:28:54 -0700

    bashrc: fix dircolors for MacOS

--- home/.bashrc.d/80-dircolors.bash
+++ home/.bashrc.d/80-dircolors.bash
@@ -1,6 +1,10 @@
 # solarized dircolors
 if [ -d $HOME/.homesick/repos/dircolors-solarized ]; then
     if [[ $TERM == *-256color ]]; then
-        eval `dircolors $HOME//.homesick/repos/dircolors-solarized/dircolors.256dark`
+        if hash gdircolors 2> /dev/null; then
+            eval `gdircolors $HOME/.homesick/repos/dircolors-solarized/dircolors.256dark`
+        elif hash dircolors 2> /dev/null; then
+            eval `dircolors $HOME/.homesick/repos/dircolors-solarized/dircolors.256dark`
+        fi
     fi
 fi
