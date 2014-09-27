7ec65a7086c5851be22f0c6dd59b958859c28fad
Kenny Root <kenny@the-b.org>
2014-09-27 15:46:14 -0700

    bash: make Android tree switching a function

--- home/.bashrc.d/10-setup-android-env.bash
+++ home/.bashrc.d/10-setup-android-env.bash
@@ -18,46 +18,64 @@ clean_up() {
     fi
 }
 
-if [[ ! -z ${ANDROID_DIR} && -d ${ANDROID_DIR} ]]; then
-    declare -a targets
+get_term_size_with_max() {
+    local width height
+    local max_height max_width
 
-    for dir in $(find $ANDROID_DIR -maxdepth 1 '(' -type d -or -type l ')' -and '!' -name '.*' -print); do
-        trimmed_dir="${dir#$ANDROID_DIR}"
-        if [[ -n ${trimmed_dir} ]]; then
-            targets+=("$trimmed_dir")
-        fi
-    done
+    max_height="$1"
+    max_width="$2"
 
-    target=""
-    if (( ${#targets[@]} == 1 )); then
-        target="${targets[0]}"
-    else
-        dialog_cmd="dialog --output-fd 5 --menu Targets: 15 40 ${#targets[@]} "
-        readarray -t sorted < <(printf '%s\0' "${targets[@]}" | sort -z | xargs -0n1)
-        for (( i = 0; i < ${#sorted[@]}; i++ )); do
-            dialog_cmd="${dialog_cmd} $i ${sorted[$i]}"
+    read height width < <(stty size)
+
+    echo $(($height>$max_height?$max_height:$height)) $(($width>$max_width?$max_width:$width))
+}
+
+switch_android_tree() {
+    local -a targets
+    local trimmed_dir target dialog_cmd ret tmp_file
+
+    if [[ -n ${ANDROID_DIR} && -d ${ANDROID_DIR} ]]; then
+
+        for dir in $(find $ANDROID_DIR -maxdepth 1 '(' -type d -or -type l ')' -and '!' -name '.*' -print); do
+            trimmed_dir="${dir#$ANDROID_DIR}"
+            if [[ -n ${trimmed_dir} ]]; then
+                targets+=("$trimmed_dir")
+            fi
         done
-        tmp_file="$(tempfile)"
-        trap 'clean_up "${tmp_file}"' EXIT SIGINT SIGQUIT SIGTERM
-        tput smcup
-        eval $dialog_cmd 5> ${tmp_file}
-        (( ret = $? ))
-        tput rmcup
-        if (( ret == 0 )); then
-            clear
-            read target_number < ${tmp_file}
-            target="${sorted[$target_number]}"
+
+        target=""
+        if (( ${#targets[@]} == 1 )); then
+            target="${targets[0]}"
+        else
+          dialog_cmd="dialog --output-fd 5 --menu Targets: $(get_term_size_with_max $((${#targets[@]}+7)) 40) ${#targets[@]} "
+            readarray -t sorted < <(printf '%s\0' "${targets[@]}" | sort -z | xargs -0n1)
+            for (( i = 0; i < ${#sorted[@]}; i++ )); do
+                dialog_cmd="${dialog_cmd} $i ${sorted[$i]}"
+            done
+            tmp_file="$(tempfile)"
+            trap 'clean_up "${tmp_file}"' EXIT SIGINT SIGQUIT SIGTERM
+            tput smcup
+            eval $dialog_cmd 5> ${tmp_file}
+            (( ret = $? ))
+            tput rmcup
+            if (( ret == 0 )); then
+                clear
+                read target_number < ${tmp_file}
+                target="${sorted[$target_number]}"
+            fi
+        fi
+        if [[ -n ${target} ]]; then
+            export TOP="${ANDROID_DIR}${target}"
+            export CCACHE_BASEDIR="${TOP}"
+            export CDPATH="${CDPATH}:${ANDROID_DIR}"
+            pushd "${TOP}" > /dev/null 2>&1
+            . "${TOP}/build/envsetup.sh"
+            popd > /dev/null 2>&1
         fi
     fi
-    if [[ -n ${target} ]]; then
-        export TOP="${ANDROID_DIR}${target}"
-        export CCACHE_BASEDIR="${TOP}"
-        export CDPATH="${CDPATH}:${ANDROID_DIR}"
-        pushd "${TOP}" > /dev/null 2>&1
-        . "${TOP}/build/envsetup.sh"
-        popd > /dev/null 2>&1
-    fi
-fi
 
-trap - EXIT SIGINT SIGQUIT SIGTERM
-clean_up "${tmp_file}"
+    trap - EXIT SIGINT SIGQUIT SIGTERM
+    clean_up "${tmp_file}"
+}
+
+switch_android_tree
