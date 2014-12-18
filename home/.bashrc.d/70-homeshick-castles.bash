42decdf11814eeff987bd782eb440657787d1f32
Kenny Root <kenny@the-b.org>
2014-12-18 12:44:09 -0800

    bash: remove scm_breeze for now
    The rewriting of the command line doesn't work most of the time.

--- home/.bashrc.d/70-homeshick-castles.bash
+++ home/.bashrc.d/70-homeshick-castles.bash
@@ -2,4 +2,8 @@
 homeshick clone --quiet --batch git@github.com:kruton/dotfiles
 homeshick clone --quiet --batch https://github.com/Shougo/neobundle.vim.git
 homeshick clone --quiet --batch https://github.com/huyz/dircolors-solarized
-homeshick clone --quiet --batch https://github.com/ndbroadbent/scm_breeze.git
+
+#homeshick clone --quiet --batch https://github.com/ndbroadbent/scm_breeze.git
+if [ -d $HOME/.homesick/repos/scm_breeze ]; then
+  rm -rf $HOME/.homesick/repos/scm_breeze
+fi
