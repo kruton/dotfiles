12088e028c5719a5edcedd488492fdaf7729a145
Kenny Root <kenny@the-b.org>
2015-06-16 09:22:20 -0700

    tmux-notify: tweak the script

--- home/bin/tmux-notify.sh
+++ home/bin/tmux-notify.sh
@@ -10,20 +10,20 @@ then
 fi
  
 # Retrieve information about tmux
-WINDOW=`/usr/bin/tmux display -pt $TMUX_PANE '#S:#I' 2> /dev/null`
-IS_WINDOW_ACTIVE=`/usr/bin/tmux display -pt $TMUX_PANE '#{window_active}' 2> /dev/null`
+WINDOW=`tmux display -pt $TMUX_PANE '#S:#I' 2> /dev/null`
+IS_WINDOW_ACTIVE=`tmux display -pt $TMUX_PANE '#{window_active}' 2> /dev/null`
  
 if [ "$IS_WINDOW_ACTIVE" == "0" ] && [ "$WINDOW" != "" ]
 then
     # Enable visual activity
-    /usr/bin/tmux set-window-option -t $WINDOW monitor-activity on > /dev/null
+    tmux set-window-option -t $WINDOW monitor-activity on > /dev/null
  
     # Print nothing, but this triggers the activity monitor. Requires bash to not fuck up the output.
     echo -ne "\0"
  
     # Alternative, but not tested
-    # /usr/bin/tmux send-keys -t $WINDOW Space C-H
+    # tmux send-keys -t $WINDOW Space C-H
  
     # Disable visual activity again
-    /usr/bin/tmux set-window-option -t $WINDOW monitor-activity off > /dev/null
+    tmux set-window-option -t $WINDOW monitor-activity off > /dev/null
 fi
