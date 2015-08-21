3f7a186b204629d4f723d7c4afb56cae012dd46b
Kenny Root <kenny@the-b.org>
2015-08-20 22:06:48 -0700

    switch from rvm to rbenv

---
+++ home/.bashrc.d/60-rbenv.bash
@@ -0,0 +1,5 @@
+# rbenv - Ruby Environment Manager
+
+if hash rbenv 2> /dev/null; then \
+    eval "$(rbenv init -)"
+fi
--- home/.bashrc.d/60-rvm.bash
+++
@@ -1,9 +0,0 @@
-# Ruby version manager
-
-if [[ -s "$HOME/.rvm/scripts/rvm" ]]; then
-    source "$HOME/.rvm/scripts/rvm"
-fi
-
-if [[ -d $HOME/.rvm/bin ]]; then
-    export PATH=$PATH:$HOME/.rvm/bin
-fi
