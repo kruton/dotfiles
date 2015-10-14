3759882b1ad4fbba01d5ba3d12c5ea337564d9a1
Kenny Root <kenny@the-b.org>
2015-10-14 12:13:40 -0700

    Update oh-my-fish repo location

--- home/.bashrc.d/70-homeshick-castles.bash
+++ home/.bashrc.d/70-homeshick-castles.bash
@@ -2,7 +2,7 @@
 homeshick clone --quiet --batch git@github.com:kruton/dotfiles
 homeshick clone --quiet --batch https://github.com/Shougo/neobundle.vim.git
 homeshick clone --quiet --batch https://github.com/huyz/dircolors-solarized
-homeshick clone --quiet --batch https://github.com/bpinto/oh-my-fish.git
+homeshick clone --quiet --batch https://github.com/oh-my-fish/oh-my-fish.git
 
 #homeshick clone --quiet --batch https://github.com/ndbroadbent/scm_breeze.git
 if [ -d $HOME/.homesick/repos/scm_breeze ]; then
