cd4f2f594e638efacd5e7abacec677816bb09708
Kenny Root <kenny@the-b.org>
2015-11-09 16:09:12 +0900

    fish: hide user in powerline

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -12,5 +12,6 @@ set fish_custom $HOME/.homesick/repos/dotfiles/oh-my-fish
 
 . "$HOME/.homesick/repos/homeshick/homeshick.fish"
 
+set POWERLINE_COMMAND_ARGS -t kruton.segment_data.user.args.hide_user="$DEFAULT_USER"
 set fish_function_path $fish_function_path "$HOME/.vim/bundle/powerline/powerline/bindings/fish"
 powerline-setup
