fdb8eb422a75d5f86b3630a803744b5ea3a40560
Kenny Root <kenny@the-b.org>
2015-12-28 17:11:28 -0600

    Add quoting for bash completion

--- home/.bashrc.d/51-bash-completion-homebrew.bash
+++ home/.bashrc.d/51-bash-completion-homebrew.bash
@@ -1,6 +1,6 @@
 if [[ $OSTYPE == darwin* ]]; then
     # Put Homebrew BASH completion in
-    if [ -f $(brew --prefix)/etc/profile.d/bash_completion.sh ]; then
-        . $(brew --prefix)/etc/profile.d/bash_completion.sh
+    if [ -f "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]; then
+        . "$(brew --prefix)/etc/profile.d/bash_completion.sh"
     fi
 fi
--- home/.bashrc.d/51-bash-completion-macports.bash
+++ home/.bashrc.d/51-bash-completion-macports.bash
@@ -1,6 +1,6 @@
 if [[ $OSTYPE == darwin* && -n $MP_PREFIX ]]; then
     # Put Homebrew BASH completion in
-    if [ -f $MP_PREFIX/etc/profile.d/bash_completion.sh ]; then
-        . $MP_PREFIX/etc/profile.d/bash_completion.sh
+    if [ -f "$MP_PREFIX/etc/profile.d/bash_completion.sh" ]; then
+        . "$MP_PREFIX/etc/profile.d/bash_completion.sh"
     fi
 fi
