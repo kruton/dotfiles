8b165a317bed246a9e19040ee58e57d8e887bad4
Kenny Root <kenny@the-b.org>
2016-02-01 10:53:48 +0000

    bash: completion should check for brew first

--- home/.bashrc.d/51-bash-completion-homebrew.bash
+++ home/.bashrc.d/51-bash-completion-homebrew.bash
@@ -2,7 +2,9 @@
 
 if [[ $OSTYPE == darwin* ]]; then
     # Put Homebrew BASH completion in
-    if [ -f "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]; then
-        . "$(brew --prefix)/etc/profile.d/bash_completion.sh"
+    if hash brew 2> /dev/null; then \
+        if [ -f "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]; then
+            . "$(brew --prefix)/etc/profile.d/bash_completion.sh"
+        fi
     fi
 fi
--- home/.bashrc.d/51-bash-completion-macports.bash
+++ home/.bashrc.d/51-bash-completion-macports.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ $OSTYPE == darwin* && -n $MP_PREFIX ]]; then
-    # Put Homebrew BASH completion in
+    # Put MacPorts BASH completion in
     if [ -f "$MP_PREFIX/etc/profile.d/bash_completion.sh" ]; then
         . "$MP_PREFIX/etc/profile.d/bash_completion.sh"
     fi
