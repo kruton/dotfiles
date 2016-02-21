6eec86ce74518af5547e9deebd9974af9ec56b67
Kenny Root <kenny@the-b.org>
2016-02-21 12:49:41 -0800

    bash: give into MacPorts sucking
    For some reason the git bash_completion was not installed into any place
    that auto-loaded.

--- home/.bashrc.d/51-bash-completion-macports.bash
+++ home/.bashrc.d/51-bash-completion-macports.bash
@@ -5,4 +5,8 @@ if [[ $OSTYPE == darwin* && -n $MP_PREFIX ]]; then
     if [ -f "$MP_PREFIX/etc/profile.d/bash_completion.sh" ]; then
         . "$MP_PREFIX/etc/profile.d/bash_completion.sh"
     fi
+
+    if [ -f "$MP_PREFIX/share/git/contrib/completion/git-completion.bash" ]; then
+        . "$MP_PREFIX/share/git/contrib/completion/git-completion.bash"
+    fi
 fi
