5ef7c529669b2b84357e6f5caf3b601fdcdfac98
Kenny Root <kenny@the-b.org>
2016-05-28 22:46:27 -0700

    bash: setup rbenv and ruby-build if present

--- home/.bashrc.d/60-rbenv.bash
+++ home/.bashrc.d/60-rbenv.bash
@@ -2,6 +2,14 @@
 
 # rbenv - Ruby Environment Manager
 
+if [ -d "$HOME/.rbenv" ]; then
+    export PATH="$PATH:$HOME/.rbenv/bin"
+fi
+
+if [ -d "$HOME/.ruby-build/bin" ]; then
+    export PATH="$PATH:$HOME/.ruby-build/bin"
+fi
+
 if hash rbenv 2> /dev/null; then \
     eval "$(rbenv init -)"
 fi
