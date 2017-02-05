a1d5817fb1843d01ba815f9542e7e4d09ac556d0
Kenny Root <kenny@the-b.org>
2017-02-05 09:43:48 -0800

    Follow MacPorts preferred Python

--- home/.bashrc.d/49-macports-path.bash
+++ home/.bashrc.d/49-macports-path.bash
@@ -4,7 +4,8 @@
 if [[ $OSTYPE == darwin* ]]; then
     export MP_PREFIX=$HOME/macports
 
-    export PATH=$MP_PREFIX/bin:$MP_PREFIX/sbin:$MP_PREFIX/Library/Frameworks/Python.framework/Versions/2.7/bin:$PATH
+    export PATH=$MP_PREFIX/bin:$MP_PREFIX/sbin:$PATH
+    export PATH=`python -c 'import site; print(site.PREFIXES[0])'`/bin:$PATH
     export MANPATH=$MP_PREFIX/share/man:$MANPATH
     export PERL5LIB=$MP_PREFIX/lib/perl5/5.12.4:$MP_PREFIX/lib/perl5/vendor_perl/5.12.4:$PERL5LIB
 fi
