7f41e3234c634ec38e0e8b6654bb5d7278d7d337
Kenny Root <kenny@the-b.org>
2017-09-05 10:43:00 +0900

    Add bash-preexec castle

--- home/.bashrc.d/70-homeshick-castles.bash
+++ home/.bashrc.d/70-homeshick-castles.bash
@@ -2,6 +2,7 @@
 
 # All the castles I want
 homeshick clone --quiet --batch git@github.com:kruton/dotfiles
+homeshick clone --quiet --batch https://github.com/rcaloras/bash-preexec.git
 homeshick clone --quiet --batch https://github.com/Shougo/neobundle.vim.git
 homeshick clone --quiet --batch https://github.com/huyz/dircolors-solarized
 homeshick clone --quiet --batch https://github.com/fisherman/fisherman.git
