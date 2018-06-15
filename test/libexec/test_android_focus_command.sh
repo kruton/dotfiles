181525268317310cbf8deb582c4b011256981acf
Kenny Root <kenny@the-b.org>
2018-06-14 23:06:11 -0700

    Fix shellcheck for test script

--- test/libexec/test_android_focus_command.sh
+++ test/libexec/test_android_focus_command.sh
@@ -3,22 +3,25 @@
 # Using this:
 #   test_android_focus_command.sh <string> <previous serial>
 #
-REPO_DIR="$( cd -P "$( dirname $0 )/../.." && pwd )"
+REPO_DIR="$( cd -P "$( dirname "$0" )/../.." && pwd )"
 
 # Set up the completion environment. Not really needed for "focus" but
 # is needed to source the file below.
-command -v brew > /dev/null 2>&1
-if [ $? -eq 0 ]; then \
+if command -v brew > /dev/null 2>&1; then \
+    # shellcheck source=/dev/null
     source /usr/local/share/bash-completion/bash_completion
 elif [ -f /usr/share/bash-completion/bash_completion ]; then \
+    # shellcheck source=/dev/null
     source /usr/share/bash-completion/bash_completion
 else \
     # Wait, why is there a prefix here?
+    # shellcheck source=/dev/null
     source ${prefix}/etc/bash_completion
 fi
-source ${REPO_DIR}/home/.bashrc.d/20-android-focus.bash
+# shellcheck source=/dev/null
+source "${REPO_DIR}/home/.bashrc.d/20-android-focus.bash"
 
 ANDROID_SERIAL=$1
 echo "[START serial=$ANDROID_SERIAL]"
-focus $2
+focus "$2"
 echo "[END serial=$ANDROID_SERIAL]"
--- test/libexec/test_android_focus_completion.sh
+++ test/libexec/test_android_focus_completion.sh
@@ -3,21 +3,21 @@
 # Using this:
 #   test_android_focus_completion.sh <string> <cursor pos> <word pos> [<string> <cursor pos> <word pos> ...]
 #
-REPO_DIR="$( cd -P "$( dirname $0 )/../.." && pwd )"
+REPO_DIR="$( cd -P "$( dirname "$0" )/../.." && pwd )"
 FIXTURES_DIR="${REPO_DIR}/test/fixtures/"
 
 __print_completions() {
     for ((i=0;i<${#COMPREPLY[*]};i++))
     do
-       echo ${COMPREPLY[i]}
+       echo "${COMPREPLY[i]}"
     done
 }
 
 assert_fixture() {
   local cmd
-  cmd="$(type -p $1)"
+  cmd="$(type -p "$1")"
   if [ "${cmd#${FIXTURES_DIR}}" = "${cmd}" ]; then
-    echo $PATH
+    echo "$PATH"
     echo "$cmd is not a fixture"
     exit 0
   fi
@@ -26,16 +26,19 @@ assert_fixture adb
 assert_fixture fastboot
 
 # Set up the completion environment
-command -v brew > /dev/null 2>&1
-if [ $? -eq 0 ]; then \
+if command -v brew > /dev/null 2>&1; then \
+    # shellcheck source=/dv/null
     source /usr/local/share/bash-completion/bash_completion
 elif [ -f /usr/share/bash-completion/bash_completion ]; then \
+    # shellcheck source=/dv/null
     source /usr/share/bash-completion/bash_completion
 else \
     # Wait, why is there a prefix here?
-    source ${prefix}/etc/bash_completion
+    # shellcheck source=/dv/null
+    source "${prefix}/etc/bash_completion"
 fi
-source ${REPO_DIR}/home/.bashrc.d/20-android-focus.bash
+# shellcheck source=/dv/null
+source "${REPO_DIR}/home/.bashrc.d/20-android-focus.bash"
 
 if (( ($# % 3) != 0 )); then
   echo "Expected multiples of 3 arguments but $# found."
@@ -44,7 +47,7 @@ fi
 
 loop=0
 while (( $# > 0 )); do
-  read -a COMP_WORDS <<< "$1"
+  read -r -a COMP_WORDS <<< "$1"
   COMP_LINE="$1"
   COMP_POINT="$2"
   COMP_CWORD="$3"
