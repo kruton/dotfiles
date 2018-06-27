4e5ed1ef1d0b56d64acc26a2d60e820ca65c26dd
Kenny Root <kenny@the-b.org>
2018-06-27 14:20:45 +0900

    Android focus: also use fzf if possible

--- home/.bashrc.d/10-setup-android-env.bash
+++ home/.bashrc.d/10-setup-android-env.bash
@@ -33,36 +33,43 @@ get_term_size_with_max() {
 
 switch_android_tree() {
     local -a targets
-    local trimmed_dir target dialog_cmd ret tmp_file
+    local trimmed_dir target ret tmp_file
     local dir
 
-    if [[ -n ${ANDROID_DIR} && -d ${ANDROID_DIR} ]]; then
-        while IFS= read -r -d '' dir; do
-            trimmed_dir="${dir#$ANDROID_DIR}"
+    if [[ -n $ANDROID_DIR && -d $ANDROID_DIR ]]; then
+        while IFS= read -r -d $'\0' dir; do
+            trimmed_dir="${dir#$ANDROID_DIR/}"
             if [[ -n ${trimmed_dir} ]]; then
                 targets+=("$trimmed_dir")
             fi
-        done < <(find "$ANDROID_DIR" -maxdepth 1 '(' -type d -or -type l ')' -and '!' -name '.*' -print0)
+        done < <(shopt -s nullglob; for f in $ANDROID_DIR/*; do printf "%s\0" "$f"; done)
 
         target=""
         if (( ${#targets[@]} == 1 )); then
             target="${targets[0]}"
         else
-            dialog_cmd="dialog --output-fd 5 --menu Targets: $(get_term_size_with_max $((${#targets[@]}+7)) 40) ${#targets[@]} "
-            readarray -t sorted < <(printf '%s\0' "${targets[@]}" | sort -z | xargs -0n1)
-            for (( i = 0; i < ${#sorted[@]}; i++ )); do
-                dialog_cmd="${dialog_cmd} $i ${sorted[$i]}"
-            done
-            tmp_file="$(mktemp)"
-            trap 'clean_up "${tmp_file}"' EXIT SIGINT SIGQUIT SIGTERM
-            tput smcup
-            eval "$dialog_cmd" 5> "$tmp_file"
-            (( ret = $? ))
-            tput rmcup
-            if (( ret == 0 )); then
-                clear
-                read -r target_number < "$tmp_file"
-                target="${sorted[$target_number]}"
+            if command -v fzf > /dev/null; then
+                target=$(fzf-tmux +m -0 -1 --read0 < <(printf "%s\0" "${targets[@]}"))
+            else
+                local dialog_cmd
+                local tmp_file
+
+                readarray -t sorted < <(printf '%s\0' "${targets[@]}" | sort -z | xargs -0n1)
+                dialog_cmd="dialog --output-fd 5 --menu Targets: $(get_term_size_with_max $((${#sorted[@]}+7)) 40) ${#sorted[@]} "
+                for (( i = 0; i < ${#sorted[@]}; i++ )); do
+                    dialog_cmd="${dialog_cmd} $i ${sorted[$i]}"
+                done
+                tmp_file="$(mktemp)"
+                trap 'clean_up "${tmp_file}"' EXIT SIGINT SIGQUIT SIGTERM
+                tput smcup
+                eval "$dialog_cmd" 5> "$tmp_file"
+                (( ret = $? ))
+                tput rmcup
+                if (( ret == 0 )); then
+                    clear
+                    read -r target_number < "$tmp_file"
+                    target="${sorted[$target_number]}"
+                fi
             fi
         fi
         if [[ -n ${target} ]]; then
