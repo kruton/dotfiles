d632f50a45a9203f5ea7c2099380c4818ecc3199
Kenny Root <kroot@google.com>
2020-02-04 07:53:54 -0800

    powerline: use pip3 as well
    Python 3 is the future!

--- home/.bashrc.d/72-powerline-install.bash
+++ home/.bashrc.d/72-powerline-install.bash
@@ -4,4 +4,7 @@
 
 if [[ ! -x $HOME/.local/bin/powerline ]]; then \
     pip install --user --editable="$HOME/.homesick/repos/powerline"
+    if command -v pip3 > /dev/null 2>&1; then \
+        pip3 install --user --editable="$HOME/.homesick/repos/powerline"
+    fi
 fi
