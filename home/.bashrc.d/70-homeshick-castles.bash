c331f0b9aec2ec95b2a201b28c9a62bd37c82869
Kenny Root <kenny@the-b.org>
2021-10-21 20:36:46 -0700

    bashrc: get rid of fisherman
    This repo is apparently the center of drama. It has diverged in the git
    repo several times since it has been in my dotfiles. There were two
    owners that both claimed ownership. The main branch name changed. The
    git URL changed.
    
    Basically anything you can think of when wondering how to do a public
    distribution of software wrong has happened.

--- home/.bashrc.d/70-homeshick-castles.bash
+++ home/.bashrc.d/70-homeshick-castles.bash
@@ -5,7 +5,6 @@ homeshick clone --quiet --batch git@github.com:kruton/dotfiles
 homeshick clone --quiet --batch https://github.com/rcaloras/bash-preexec.git
 homeshick clone --quiet --batch https://github.com/Shougo/neobundle.vim.git
 homeshick clone --quiet --batch https://github.com/huyz/dircolors-solarized
-homeshick clone --quiet --batch https://github.com/fisherman/fisherman.git
 homeshick clone --quiet --batch https://github.com/powerline/powerline.git
 homeshick clone --quiet --batch https://github.com/okraits/j4-make-config
 homeshick clone --quiet --batch https://github.com/nornagon/git-rebase-all.git
@@ -20,3 +19,9 @@ homeshick clone --quiet --batch https://github.com/lincheney/fzf-tab-completion.
 #homeshick clone --quiet --batch https://github.com/oh-my-fish/oh-my-fish.git
 [ -d "$HOME/.homesick/repos/oh-my-fish" ] && \
     rm -rf "$HOME/.homesick/repos/oh-my-fish"
+
+# This project is a total joke; drama spills out onto the git
+# repo repeatedly.
+#homeshick clone --quiet --batch https://github.com/fisherman/fisherman.git
+[ -d "$HOME/.homesick/repos/fisherman" ] && \
+    rm -rf "$HOME/.homesick/repos/fisherman"
