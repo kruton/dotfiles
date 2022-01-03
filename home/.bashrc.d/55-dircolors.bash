3b847994394e6d5f0ec74a3e5b8b81e141b61c7f
Kenny Root <kenny@the-b.org>
2022-01-03 15:18:49 -0800

    Support g* prefix for homebrew

--- home/.bashrc.d/55-dircolors.bash
+++ home/.bashrc.d/55-dircolors.bash
@@ -2,5 +2,9 @@
 # Sets DIR_COLORS, et al., if not already set.
 
 if [[ -z $DIR_COLORS && -z $LS_COLORS ]]; then
-	eval "$(dircolors)"
+	if command -v gdircolors > /dev/null; then \
+		eval "$(gdircolors)"
+	else \
+		eval "$(dircolors)"
+	fi
 fi
