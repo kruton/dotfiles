9464fb7bfcb981e68c4c9bdc36e991df6aea34c3
Kenny Root <kenny@the-b.org>
2024-02-27 11:22:56 -0800

    docker-tags: add command

---
+++ home/.bashrc.d/52-docker-tags.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+docker-tags() {
+    arr=("$@")
+
+    for item in "${arr[@]}";
+    do
+        tokenUri="https://auth.docker.io/token"
+        data=("service=registry.docker.io" "scope=repository:$item:pull")
+        token="$(curl --silent --get --data-urlencode ${data[0]} --data-urlencode ${data[1]} $tokenUri | jq --raw-output '.token')"
+        listUri="https://registry-1.docker.io/v2/$item/tags/list"
+        authz="Authorization: Bearer $token"
+        result="$(curl --silent --get -H "Accept: application/json" -H "Authorization: Bearer $token" $listUri | jq --raw-output '.')"
+        echo $result
+    done
+}
