87cabc5370fade255a33a0a33b72eee717eeac15
Kenny Root <kenny@the-b.org>
2024-11-18 13:23:43 -0800

    tests: fix kcov run

--- test/docker-run.sh
+++ test/docker-run.sh
@@ -5,11 +5,13 @@ REPO_BASE="${1:-$DETECTED_BASE}"
 
 docker run --rm --security-opt seccomp=unconfined \
     -v "$REPO_BASE:$REPO_BASE" \
-    -e TERM=xterm kcov \
-    kcov --clean . --include-path="$REPO_BASE/" \
+    -e TERM=xterm -t \
+    --ulimit nofile=1024:1024 \
+    kcov \
+    kcov --clean --include-path="$REPO_BASE/" \
     --exclude-path="$REPO_BASE/test/,$REPO_BASE/bats/" \
     --strip-path="$REPO_BASE" \
-    -- \
+    . \
     "$REPO_BASE/bats/bin/bats" --tap "$REPO_BASE/test"
 
 perl -pi -e 's,/(</source>),\1,g' $REPO_BASE/coverage/bats/cobertura.xml
