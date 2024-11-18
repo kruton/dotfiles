3c74d5550b3d117bc4bf9d7a210454bf346d3f67
Kenny Root <kenny@the-b.org>
2024-11-18 14:43:11 -0800

    kubernetes: auto update flux

--- home/.bashrc.d/91-kubernetes.bash
+++ home/.bashrc.d/91-kubernetes.bash
@@ -5,6 +5,20 @@ if [ -x $HOME/bin/kubectl ]; then
     . <($HOME/bin/kubectl completion bash)
 fi
 
+_check_flux_version() {
+    # renovate: datasource=github-releases depName=fluxcd/flux2 versioning=semver-coerced
+    local __flux_version=2.4.0
+
+    local __current_version="$($HOME/bin/flux --version)"
+    if [[ $__flux_version != ${__current_version#flux version } && $OSTYPE == linux-gnu  ]]; then \
+        url="https://github.com/fluxcd/flux2/releases/download/v${__flux_version}/flux_${__flux_version}_linux_amd64.tar.gz"
+        curl -sL "$url" | tar zxvfC - "$HOME/bin"
+    fi
+}
+
 if [ -x $HOME/bin/flux ]; then
+    _check_flux_version
     . <($HOME/bin/flux completion bash)
 fi
+
+unset -f _check_flux_version
