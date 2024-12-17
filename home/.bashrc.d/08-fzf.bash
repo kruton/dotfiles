b914f3010dd05c6b67c2b97fef3d61a23d4a7393
Kenny Root <kenny@the-b.org>
2024-12-17 14:24:33 -0800

    Move history to atuin

--- home/.bashrc.d/08-fzf.bash
+++ home/.bashrc.d/08-fzf.bash
@@ -47,39 +47,7 @@ if [[ -d "$HOME/.fzf/bin" ]]; then \
   _check_fzf_version
   export PATH="$PATH:$HOME/.fzf/bin"
 
-  # shellcheck source=/dev/null
-  #[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
-
-  # shellcheck source=/dev/null
-  source "$HOME/.fzf/shell/key-bindings.bash"
-
   _gen_fzf_default_opts
-
-  if [[ ! -z "$ALL_HISTORY_FILE" && -f "$ALL_HISTORY_FILE" ]]; then
-    # Get history.sh working with fzf
-    bind '"\C-r": "\C-x1\e^\er"'
-    bind -x '"\C-x1": __fzf_history__';
-
-    __fzf_history__() {
-      __ehc__ "$(
-        while IFS=$'\t' read -r -d $'\0' _ _ _ cmd; do
-          printf "%s" "$cmd"
-        done < "$ALL_HISTORY_FILE" | fzf --tac --scheme=history --bind=ctrl-r:toggle-sort --wrap-sign '"$'\t'"↳ ' --highlight-line +m
-      )"
-    }
-
-    __ehc__() {
-      if [[ -n $1 ]]; then
-        bind '"\er": redraw-current-line'
-        bind '"\e^": magic-space'
-        READLINE_LINE=${READLINE_LINE:+${READLINE_LINE:0:READLINE_POINT}}${1}${READLINE_LINE:+${READLINE_LINE:READLINE_POINT}}
-        READLINE_POINT=$(( READLINE_POINT + ${#1} ))
-      else
-        bind '"\er":'
-        bind '"\e^":'
-      fi
-    }
-  fi
 fi
 
 unset -f _check_fzf_version
--- home/.bashrc.d/91-atuin.bash
+++ home/.bashrc.d/91-atuin.bash
@@ -1,3 +1,20 @@
+_check_atuin_version() {
+  # renovate: datasource=github-releases depName=atuinsh/atuin
+  local __atuin_version=18.3.0
+
+  if [[ -x "$HOME/bin/atuin" ]]; then \
+    local __local_version
+    read -r _ __local_version < <("$HOME/bin/atuin" --version)
+    if [[ ${__local_version% *} != "$__atuin_version" ]]; then \
+      # TODO: download new version automatically
+      echo "New atuin version available: $__atuin_version"
+    fi
+  fi
+}
+
 if [[ -x $HOME/bin/atuin ]]; then \
+    _check_atuin_version
     eval "$("$HOME/bin/atuin" init bash)"
 fi
+
+unset -f _check_atuin_version
