22a942f040a7263ce51c23c59bdb5b2ad89b28b4
Kenny Root <kenny@the-b.org>
2025-05-12 17:45:52 -0700

    chezmoi: fix migration script

--- home/.bashrc.d/01-migrate-to-chezmoi.bash
+++ home/.bashrc.d/01-migrate-to-chezmoi.bash
@@ -24,8 +24,8 @@ _download_chezmoi() {
   mkdir -p "$HOME/bin"
   cd "$HOME/bin" || exit 1
   bin="chezmoi-$OS-$ARCH"
-  wget -q "https://github.com/twpayne/chezmoi/releases/download/v2.62.4/$bin"
-  sha256sum --check --ignore-missing <<EOF
+  curl -sSL -o "$bin" "https://github.com/twpayne/chezmoi/releases/download/v2.62.4/$bin"
+  shasum -a 256 --check --ignore-missing <<EOF
 07910f5c2eb9acb074a7b27833a0534b716ba2317ea9339e6a5ddb580cbc5f60  chezmoi-darwin-amd64
 e2e9ee87d47e34f87ac5dee1c582a7e11fa3dd32d51b5b32995ac1f877294b93  chezmoi-darwin-arm64
 158aee682cb6765efa19120667c3aaa67842fcaa6118441f149171b9c2eb8eb6  chezmoi-linux-amd64
@@ -37,7 +37,7 @@ EOF
   fi
   mv "$bin" chezmoi
   chmod +x chezmoi
-  ./chezmoi init --apply kruton
+  ./chezmoi init kruton --apply
 }
 
 _download_chezmoi
