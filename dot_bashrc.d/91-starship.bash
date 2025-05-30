6c9e33facb3d337bc0f4eeb7f224000fc0fa0c69
Kenny Root <kenny@the-b.org>
2025-05-30 13:08:03 -0700

    starship: split download from check

--- dot_bashrc.d/91-starship.bash
+++ dot_bashrc.d/91-starship.bash
@@ -1,13 +1,20 @@
+# renovate: datasource=github-releases depName=starship/starship
+_starship_version=1.23.0
+
+_download_starship() {
+    [[ -n $DEVSERVER ]] && return
+
+    curl -sL "https://github.com/starship/starship/releases/download/v${_starship_version}/starship-x86_64-unknown-linux-gnu.tar.gz" | tar zxfC - "$HOME/bin"
+}
+
 _check_starship_version() {
-    # renovate: datasource=github-releases depName=starship/starship
-    local starship_version=1.23.0
     local current_version
 
     [[ -n $DEVSERVER ]] && return
 
     read -r _ current_version < <(starship --version)
-    if [[ $current_version != "$starship_version" ]]; then \
-        curl -sL "https://github.com/starship/starship/releases/download/v${starship_version}/starship-x86_64-unknown-linux-gnu.tar.gz" | tar zxfC - "$HOME/bin"
+    if [[ $current_version != "$_starship_version" ]]; then \
+        _download_starship
     fi
 }
 
@@ -18,3 +25,6 @@ if command -v starship > /dev/null; then \
     eval "$(starship init bash)"
 fi
 
+unset _starship_version
+unset -f _check_starship_version
+unset -f _download_starship
