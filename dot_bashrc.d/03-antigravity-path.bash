cd7097a82bc0ece0d6da2a29af591e9420dcacd7
Kenny Root <kenny@the-b.org>
2026-06-30 19:12:58 -0700

    bashrc: add antigravity

---
+++ dot_bashrc.d/03-antigravity-path.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Added by Antigravity IDE
+if [[ -d "${HOME}/.antigravity-ide/antigravity-ide/bin" ]]; then
+    export PATH="${HOME}/.antigravity-ide/antigravity-ide/bin:$PATH"
+fi
